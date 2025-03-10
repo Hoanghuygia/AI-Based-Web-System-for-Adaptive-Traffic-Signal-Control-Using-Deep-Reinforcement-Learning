72b420cb60eedd54391eac3e8d8ac75b8959cb1c
Hoanghuygia <hoanghuy051230@gmail.com>
2025-03-11 00:22:18 +0700

    setup mongodb

--- core/src/core/config.py
+++ core/src/core/config.py
@@ -1,8 +1,14 @@
+import os
+
 from pydantic_settings import BaseSettings
+from starlette.datastructures import CommaSeparatedStrings
 
 class Settings(BaseSettings):
-    PROJECT_NAME: str = "API for Adaptive Traffic Signal Control Using Deep Reinforcement Learning"
+    PROJECT_NAME: str
+    ALLOWED_HOSTS: CommaSeparatedStrings = CommaSeparatedStrings(os.getenv("ALLOWED_HOSTS", ""))
     MONGODB_URL: str  
+    MAX_CONNECTIONS_COUNT: int = 10
+    MIN_CONNECTIONS_COUNT: int = 10
     DB_NAME: str     
     SECRET_KEY: str   
     
--- core/src/main.py
+++ core/src/main.py
@@ -2,12 +2,24 @@ from fastapi import FastAPI, status
 from starlette.exceptions import HTTPException
 from starlette.middleware.cors import CORSMiddleware
 
-# from core.core.config import settings
 from src.core.config import settings
+from src.db.mongodb_utils import close_mongo_connection, open_mongo_connection
 
 app = FastAPI(title=settings.PROJECT_NAME)
 
-app.add_middleware(CORSMiddleware)
+if not settings.ALLOWED_HOSTS:
+    ALLOWED_HOSTS = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=ALLOWED_HOSTS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.add_event_handler("startup", open_mongo_connection)
+app.add_event_handler("shutdown", close_mongo_connection)
 
 
 @app.get("/")
