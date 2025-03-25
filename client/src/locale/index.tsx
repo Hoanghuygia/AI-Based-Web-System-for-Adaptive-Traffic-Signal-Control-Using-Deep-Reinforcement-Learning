b0322b5f5037d13113d64a1bbbda0376d3651510
Hoanghuygia <hoanghuy051230@gmail.com>
2025-03-25 22:40:16 +0700

    add config for relative path

--- client/src/locale/index.tsx
+++ client/src/locale/index.tsx
@@ -1,4 +1,39 @@
-import React, { FC } from "react";
+// import React, { FC } from "react";
+// import en_US from "./en-US";
+// import vi_VN from "./vi-VN";
+// import { MessageDescriptor, FormattedMessage, useIntl } from "react-intl";
+
+// export const localeConfig = {
+//   en_US: en_US,
+//   vi_VN: vi_VN,
+// };
+
+// type Id = keyof typeof en_US;
+
+// interface Props extends MessageDescriptor {
+//   id: Id;
+// }
+
+// export const LocaleFormatter: FC<Props> = ({ children, ...props }) => {
+//   return <FormattedMessage {...props} />;
+// };
+
+// type FormatMessageProps = (
+//   descriptor: MessageDescriptor,
+//   values?: Record<string, React.ReactNode>
+// ) => string;
+
+// export const useLocale = () => {
+//   const { formatMessage: _formatMessage, ...rest } = useIntl();
+//   const formatMessage: FormatMessageProps = (descriptor, values) =>
+//     _formatMessage(descriptor, values);
+//   return {
+//     ...rest,
+//     formatMessage,
+//   };
+// };
+
+import React from "react";
 import en_US from "./en-US";
 import vi_VN from "./vi-VN";
 import { MessageDescriptor, FormattedMessage, useIntl } from "react-intl";
@@ -10,12 +45,13 @@ export const localeConfig = {
 
 type Id = keyof typeof en_US;
 
-interface Props extends MessageDescriptor {
+interface Props extends Omit<MessageDescriptor, 'id'> {
   id: Id;
+  children?: React.ReactNode;
 }
 
-export const LocaleFormatter: FC<Props> = ({ children, ...props }) => {
-  return <FormattedMessage {...props} />;
+export const LocaleFormatter: React.FC<Props> = ({ children, ...props }) => {
+  return <FormattedMessage {...props}>{children}</FormattedMessage>;
 };
 
 type FormatMessageProps = (
@@ -25,8 +61,10 @@ type FormatMessageProps = (
 
 export const useLocale = () => {
   const { formatMessage: _formatMessage, ...rest } = useIntl();
+  
   const formatMessage: FormatMessageProps = (descriptor, values) =>
     _formatMessage(descriptor, values);
+  
   return {
     ...rest,
     formatMessage,
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,7 +6,13 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname,'./src')
+      '@src': path.resolve(__dirname,'./src'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@uitl': path.resolve(__dirname, './src/uitl'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@locale': path.resolve(__dirname, './src/locale'),
+      '@mock': path.resolve(__dirname, './mock')
     }
   },
   server: {
@@ -14,7 +20,7 @@ export default defineConfig({
     open: true,
     proxy: {
       '/api': {
-        target: 'http://your-api-server.com/users', // Khi frontend gọi /apiv1/users, request sẽ được đổi thành http://your-api-server.com/users.
+        target: 'http://your-api-server.com/users', // Khi frontend gọi /api/users, request sẽ được đổi thành http://your-api-server.com/users.
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
