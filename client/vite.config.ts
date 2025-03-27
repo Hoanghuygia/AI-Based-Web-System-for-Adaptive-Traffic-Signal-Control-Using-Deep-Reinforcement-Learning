7e4ab1fcd9446ff86377399c4d0dd51b37c48fa4
Hoanghuygia <hoanghuy051230@gmail.com>
2025-03-27 22:35:29 +0700

    re-init frontend for the project

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,23 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname,'./src')
-    }
-  },
-  server: {
-    port: 5173,
-    open: true,
-    proxy: {
-      '/apiv1': {
-        target: 'http://your-api-server.com/users', // Khi frontend gọi /apiv1/users, request sẽ được đổi thành http://your-api-server.com/users.
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/apiv1/, '')
-      }
-    }
-  }
-});
+})
