7f5e33e6d38d9be6f6f5f0ee8216de33a16b8aeb
Hoanghuygia <hoanghuy051230@gmail.com>
2025-03-27 23:30:26 +0700

    setup antdesign

--- client/src/components/Test.tsx
+++ client/src/components/Test.tsx
@@ -1,5 +1,13 @@
+import { Button } from 'antd';
+
 export default function Test() {
   return (
-    <div>Test</div>
+    <>
+        <div className="App">
+            <Button type="primary">Button</Button>
+        </div>
+        <div className="text-color"> Huy abc</div> 
+        <span className='text-red-500'>TailwindCSs</span>
+    </>
   )
 }
