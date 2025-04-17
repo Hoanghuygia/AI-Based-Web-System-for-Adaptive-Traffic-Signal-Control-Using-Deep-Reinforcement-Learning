912ea82efe3a75598a2d57e7ba6daace91be2164
Hoanghuygia <hoanghuy051230@gmail.com>
2025-04-18 05:44:02 +0700

    fisnih register with bug from api

--- client/src/stores/user.slice.ts
+++ client/src/stores/user.slice.ts
@@ -1,6 +1,7 @@
 import { UserState } from "./states/UserState"
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { login as loginApi } from "@src/api/auth";
+import { register as registerApi} from "@src/api/auth";
 
 const initialUserState: UserState = {
     currentUser: null,
@@ -17,6 +18,17 @@ export const login = createAsyncThunk(
         password: string
     }) => {
         const response = await loginApi({ user: credential });
+        return response.data;
+    }
+)
+
+export const register = createAsyncThunk(
+    'user/register',
+    async(credential: {
+        username: string;
+        password: string
+    }) => {
+        const response = await registerApi({user: credential});
         return response.data;
     }
 )
@@ -42,6 +54,10 @@ export const userSlice = createSlice({
                 state.loading = true;
                 state.error = null;
             })
+            .addCase(register.pending, (state) => {
+                state.loading = true;
+                state.error = null;
+            })
             .addCase(login.fulfilled, (state, action) => {
                 console.log("Login payload:", action.payload);
                 const { username, token, refresh_token } = action.payload.user;
@@ -55,9 +71,17 @@ export const userSlice = createSlice({
                 localStorage.setItem('refresh-token', refresh_token);
                 localStorage.setItem('username', username);
             })
+            .addCase(register.fulfilled, (_, action)=> {
+                console.log("payload", action.payload);
+                console.log("Register successfully");
+            })
             .addCase(login.rejected, (state, action) => {
                 state.loading = false;
                 state.error = action.error.message || "Login failure"
+            })
+            .addCase(register.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.error.message || "Register failure"
             })
     }
 })
