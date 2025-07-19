b73b408640153c574518fa8a485e8e3bd2c3f1ae
Hoanghuygia <hoanghuy051230@gmail.com>
2025-07-20 04:00:51 +0700

    add websocket and background step function

--- core/src/api/routers.py
+++ core/src/api/routers.py
@@ -3,9 +3,11 @@ from fastapi import APIRouter
 from .endpoints.authentication import router as Authentication_route
 from .endpoints.user import router as User_route
 from .endpoints.sumo import router as Sumo_route
+from .endpoints.ws_sumo import router as SumoWebSocket_route
 
 routers = APIRouter()
 
 routers.include_router(Authentication_route)
 routers.include_router(User_route)
 routers.include_router(Sumo_route)
+routers.include_router(SumoWebSocket_route)
