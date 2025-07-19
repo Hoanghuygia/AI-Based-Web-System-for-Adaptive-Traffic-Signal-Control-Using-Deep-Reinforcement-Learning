3e3d0dc1db6432ca58758ea7b0ef40d9c5143c4d
Hoanghuygia <hoanghuy051230@gmail.com>
2025-07-20 03:20:48 +0700

    Setup traci and sumo manager

--- core/src/api/routers.py
+++ core/src/api/routers.py
@@ -2,8 +2,10 @@ from fastapi import APIRouter
 
 from .endpoints.authentication import router as Authentication_route
 from .endpoints.user import router as User_route
+from .endpoints.sumo import router as Sumo_route
 
 routers = APIRouter()
 
 routers.include_router(Authentication_route)
 routers.include_router(User_route)
+routers.include_router(Sumo_route)
