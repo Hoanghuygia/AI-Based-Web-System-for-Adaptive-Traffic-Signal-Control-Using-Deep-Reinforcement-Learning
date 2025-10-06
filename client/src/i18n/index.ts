07d114920603516c95b4c5f32eb6d7a8162c721c
Hoanghuygia <hoanghuy051230@gmail.com>
2025-10-06 21:52:02 +0700

    feat(fe): add France

--- client/src/i18n/index.ts
+++ client/src/i18n/index.ts
@@ -2,6 +2,7 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import en from './en/translation.json';
 import vi from './vi/translation.json';
+import fr from './fr/translation.json';
 
 i18n
   .use(initReactI18next)
@@ -9,8 +10,9 @@ i18n
     resources: {
       en: { translation: en },
       vi: { translation: vi },
+      fr: { translation: fr },
     },
-    lng: "vi",
+    lng: "en",
     fallbackLng: "en",
     interpolation: { escapeValue: false },
   });
