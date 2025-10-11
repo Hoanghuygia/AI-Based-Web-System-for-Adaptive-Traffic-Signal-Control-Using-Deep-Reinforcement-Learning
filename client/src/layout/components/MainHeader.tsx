b9a66eaa6b439525bd31a0bceb9c834984b32e17
Hoanghuygia <hoanghuy051230@gmail.com>
2025-10-12 01:03:50 +0700

    feat(fe): add option for header

--- client/src/layout/components/MainHeader.tsx
+++ client/src/layout/components/MainHeader.tsx
@@ -1,11 +1,10 @@
 import React from "react";
-import { Button } from "antd";
+import { Button, Dropdown } from "antd";
 import {
     EnvironmentOutlined,
     MenuOutlined,
 } from "@ant-design/icons";
 import { useTranslation } from "react-i18next";
-
 
 type MainHeaderProps = {
     username: string;
@@ -15,6 +14,21 @@ const MainHeader: React.FC<MainHeaderProps> = ({
     username,
 }) => {
     const { t } = useTranslation();
+
+    const items = [
+        {
+            key: "1",
+            label: "Profile",
+        },
+        {
+            key: "2",
+            label: "Settings",
+        },
+        {
+            key: "3",
+            label: "Logout",
+        },
+    ];
 
     return (
         <div className="flex justify-between items-center">
@@ -28,13 +42,22 @@ const MainHeader: React.FC<MainHeaderProps> = ({
                 <span className="text-gray-600">
                     {t("dashboard.welcome")}, {username}
                 </span>
+
                 <button className="cursor-pointer flex items-center justify-self-center h-8 px-4 rounded-md text-gray-600 hover:text-gray-800 hover:bg-gray-200">
                     {t("dashboard.notification")}
                 </button>
-                <Button
-                    type="text"
-                    icon={<MenuOutlined />}
-                />
+
+                <Dropdown
+                    menu={{ items }}
+                    overlayClassName="custom-dropdown"
+                    placement="bottomRight"
+                    trigger={["click"]}
+                >
+                    <Button
+                        type="text"
+                        icon={<MenuOutlined />}
+                    />
+                </Dropdown>
             </div>
         </div>
     );
