6d226c83e773293d88b59b97b7fe20a93b6fdc1e
Hoanghuygia <hoanghuy051230@gmail.com>
2025-10-14 16:08:28 +0700

    feat(fe) add sider

--- client/src/layout/MainLayout.tsx
+++ client/src/layout/MainLayout.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import { Layout, Menu, Button } from "antd";
-const { Header, Sider, Content, Footer } = Layout;
-import {
-    EnvironmentOutlined,
-    MenuOutlined,
-} from "@ant-design/icons";
+import { Layout, Menu } from "antd";
+const { Header, Sider, Content } = Layout;
 import { useAppSelector } from "@src/stores/hooks";
 import MainHeader from "@src/layout/components/MainHeader";
+import {
+    DashboardOutlined,
+    BarChartOutlined,
+    SettingOutlined,
+    ApartmentOutlined,
+} from "@ant-design/icons";
 
 type MainLayoutPros = {
     children?: React.ReactNode;
@@ -19,33 +21,65 @@ const MainLayout: React.FC<MainLayoutPros> = ({
     const username = useAppSelector(
         (state) => state.user.currentUser
     );
-    console.log("Username: " + username);
 
     return (
         <Layout className="h-dvh">
             <Header className="bg-white shadow-md sticky top-0 z-50 ">
                 <MainHeader username={username} />
             </Header>
-            <div id="container-a" className="my-8 px-12">
-                <Layout className="w-full">
+            <div id="container-a" className="my-8 px-12 h-full">
+                <Layout className="w-full h-full">
                     <Sider
-                        width={200}
-                        className="bg-white shadow-md"
+                        width={260}
+                        className="bg-purple-500 shadow-md !rounded-xl"
                         breakpoint="lg"
                         collapsedWidth="0"
                     >
                         <Menu
                             theme="dark"
                             mode="inline"
+                            className="text-white font-semibond bg-purple-500 pt-6 px-4 !rounded-lg"
                             defaultSelectedKeys={["1"]}
                             items={[
                                 {
-                                    key: "1",
-                                    label: "Dashboard",
+                                    type: "group",
+                                    label: "OVERVIEW",
+                                    children: [
+                                        {
+                                            key: "dashboard",
+                                            icon: (
+                                                <DashboardOutlined />
+                                            ),
+                                            label: "Dashboard",
+                                        },
+                                        {
+                                            key: "analytics",
+                                            icon: (
+                                                <BarChartOutlined />
+                                            ),
+                                            label: "Analytics",
+                                        },
+                                    ],
                                 },
                                 {
-                                    key: "2",
-                                    label: "Settings",
+                                    type: "group",
+                                    label: "MANAGEMENT",
+                                    children: [
+                                        {
+                                            key: "junctions",
+                                            icon: (
+                                                <ApartmentOutlined />
+                                            ),
+                                            label: "Junctions",
+                                        },
+                                        {
+                                            key: "system",
+                                            icon: (
+                                                <SettingOutlined />
+                                            ),
+                                            label: "System Settings",
+                                        },
+                                    ],
                                 },
                             ]}
                         />
